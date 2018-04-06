a8bc387147c6a99444a0712aade1b3715fa5ba6d
Zakangroth <jean.herrier@gmail.com>
2018-04-06 10:05:42 +0200

    Adding Spring Boot base project

---
+++ src/main/java/com/zakangroth/restservicesdemo/RestServicesDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zakangroth.restservicesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServicesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServicesDemoApplication.class, args);
+	}
+}
